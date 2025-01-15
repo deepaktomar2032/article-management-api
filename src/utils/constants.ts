11fba3a6dcd4571a9cf2e22afe28d657a068ba2b
deepaktomar2031 <deepaktomar2031@gmail.com>
2025-01-15 22:18:44 +0100

    Refactor environment variables in docker-compose and constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
-export const PORT = 3000
-export const SALT_VALUE = 10
-export const CACHE_DEFAULT_TIME = 600000 // 10 minutes
-export const SECRET_KEY = 'secret'
-export const EXPIRATION_TIME = '25m'
+export const PORT = Number(process.env.PORT)
+export const SALT_VALUE = Number(process.env.SALT_VALUE)
+export const CACHE_DEFAULT_TIME = Number(process.env.CACHE_DEFAULT_TIME)
+export const SECRET_KEY = process.env.SECRET_KEY
+export const EXPIRATION_TIME = process.env.EXPIRATION_TIME
