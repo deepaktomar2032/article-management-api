fbc20c3c3dff3ef5155ec90d3536a8348a80971e
deepaktomar2031 <deepaktomar2031@gmail.com>
2025-01-19 02:17:38 +0100

    Add authentication guard to comment routes & Update README

--- src/modules/comment/comment.controller.ts
+++ src/modules/comment/comment.controller.ts
@@ -1,6 +1,17 @@
-import { Body, Controller, Get, Req, Inject, Param, ParseIntPipe, Post } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Get,
+  Req,
+  Inject,
+  Param,
+  ParseIntPipe,
+  Post,
+  UseGuards,
+} from '@nestjs/common'
 import { Request } from 'express'
 import { ApiTags } from '@nestjs/swagger'
+import { AuthenticationGuard } from 'src/modules/authentication/authentication.guard'
 import { CommentService } from './comment.service'
 import { CreateCommentBody, CommentResponse, GetCommentResponse } from 'src/types'
 
@@ -10,6 +21,7 @@ export class CommentController {
   @Inject() private readonly commentService: CommentService
 
   // Create new comment
+  @UseGuards(AuthenticationGuard)
   @Post('/comment')
   async createComment(
     @Req() request: Request,
@@ -19,12 +31,14 @@ export class CommentController {
   }
 
   // Get all comments
+  @UseGuards(AuthenticationGuard)
   @Get('/comments')
   async getComments(): Promise<GetCommentResponse[]> {
     return this.commentService.getComments()
   }
 
   // Get comment by id
+  @UseGuards(AuthenticationGuard)
   @Get(`/comment/:id`)
   async getCommentById(@Param('id', ParseIntPipe) id: number): Promise<GetCommentResponse> {
     return this.commentService.getCommentById(id)
