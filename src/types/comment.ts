6c4a8f0fc7af2099261cb8906ce794d992183b11
deepaktomar2031 <deepaktomar2031@gmail.com>
2025-01-19 01:51:17 +0100

    Remove Comment type & update README

--- src/types/comment.ts
+++ src/types/comment.ts
@@ -1,12 +1,6 @@
 import { IsString, IsNotEmpty, IsNumber } from 'class-validator'
 import { CommentEntry } from './database/CommentEntry'
 
-export type Comment = {
-  authorId: number
-  articleId: number
-  content: string
-}
-
 export class CreateCommentBody {
   @IsNotEmpty()
   @IsNumber()
